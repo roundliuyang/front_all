2ce3a981439c8f19c052be569f680a47fc7bcfb3
yly <2337352929@qq.com>
2025-02-20 23:41:57 +0800

    elementUI 主题定制

--- vue-rabbit/src/App.vue
+++ vue-rabbit/src/App.vue
@@ -4,7 +4,7 @@ import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-    <el-button type="success">Success</el-button>
+  <el-button type="primary">Primary</el-button>
 </template>
 
 <style scoped>
