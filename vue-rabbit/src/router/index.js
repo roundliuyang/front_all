6375a995e625ab58e6d73ad226beada00b0e8a74
yly <2337352929@qq.com>
2025-02-26 23:32:32 +0800

    二级导航面包屑导航

--- vue-rabbit/src/router/index.js
+++ vue-rabbit/src/router/index.js
@@ -5,6 +5,7 @@ import Login from '@/views/Login/index.vue'
 import Layout from '@/views/Layout/index.vue'
 import Home from '@/views/Home/index.vue'
 import Category from '@/views/Category/index.vue'
+import SubCategory from '@/views/subCategory/index.vue'
 
 
 const router = createRouter({
@@ -22,6 +23,10 @@ const router = createRouter({
         {
           path: 'category/:id',
           component: Category
+        },
+        {
+          path: 'category/sub/:id',
+          component: SubCategory
         }
       ]
     },
