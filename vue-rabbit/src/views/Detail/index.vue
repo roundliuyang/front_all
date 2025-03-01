eec119c4a0c5cfb15d70f85c2c8d521727f314bb
yly <2337352929@qq.com>
2025-03-01 12:39:35 +0800

    热榜区

--- vue-rabbit/src/views/Detail/index.vue
+++ vue-rabbit/src/views/Detail/index.vue
@@ -3,6 +3,7 @@
 import { getDetail } from '@/apis/detail'
 import { onMounted, ref } from 'vue'
 import { useRoute } from 'vue-router'
+import DetailHot from './components/DetailHot.vue'
 
 const goods = ref({})
 const route = useRoute()
@@ -123,6 +124,10 @@ onMounted(() => getGoods())
             </div>
             <!-- 24热榜+专题推荐 -->
             <div class="goods-aside">
+              <!-- 24小时 -->
+            <DetailHot :hot-type="1"/>
+            <!-- 周 -->
+            <DetailHot :hot-type="2"/>
 
             </div>
           </div>
