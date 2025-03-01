569b6c2916354580ff7c647a45ddb15ec0026989
yly <2337352929@qq.com>
2025-03-01 20:24:51 +0800

    pinia 管理用户数据

--- vue-rabbit/src/utils/http.js
+++ vue-rabbit/src/utils/http.js
@@ -1,21 +1,28 @@
 // axios 基础的封装
 import axios from "axios";
+import { ElMessage } from 'element-plus'
+// import { useUserStore } from '@/stores/userStore'
 
 
 const httpInstance = axios.create({
-    baseURL: 'http://pcapi-xiaotuxian-front-devtest.itheima.net',
-    timeout: 5000
+  baseURL: 'http://pcapi-xiaotuxian-front-devtest.itheima.net',
+  timeout: 5000
 })
 
 // 拦截器
 // axios请求拦截器
 httpInstance.interceptors.request.use(config => {
-    return config
+  return config
 }, e => Promise.reject(e))
 
 // axios响应式拦截器
 httpInstance.interceptors.response.use(res => res.data, e => {
-    return Promise.reject(e)
+  // 统一错误提示
+  ElMessage({
+    type: 'warning',
+    message: e.response.data.message
+  })
+  return Promise.reject(e)
 })
 
 
--- vue-rabbit/src/views/Login/index.vue
+++ vue-rabbit/src/views/Login/index.vue
@@ -1,21 +1,19 @@
 <script setup>
 
 // 表单校验（账号名+密码）
-
 import { ref } from 'vue'
 
 import { ElMessage } from 'element-plus'
 import 'element-plus/theme-chalk/el-message.css'
 import { useRouter } from 'vue-router'
+import { useUserStore } from '@/stores/userStore'
 
-// import { useUserStore } from '@/stores/userStore'
-
-// const userStore = useUserStore()
+const userStore = useUserStore()
 
 // 1. 准备表单对象
 const form = ref({
-  account: '18610848230',
-  password: '123456',
+  account: '',
+  password: '',
   agree: true
 })
 
@@ -46,21 +44,20 @@ const rules = {
 
 // 3. 获取form实例做统一校验
 const formRef = ref(null)
-// const router = useRouter()
+const router = useRouter()
 const doLogin = () => {
-  // const { account, password } = form.value
+  const { account, password } = form.value
   // 调用实例方法
   formRef.value.validate(async (valid) => {
     // valid: 所有表单都通过校验  才为true
     console.log(valid)
     // 以valid做为判断条件 如果通过校验才执行登录逻辑
     if (valid) {
-      // TODO LOGIN
-      // await userStore.getUserInfo({ account, password })
+      await userStore.getUserInfo({ account, password })
       // 1. 提示用户
-      // ElMessage({ type: 'success', message: '登录成功' })
+      ElMessage({ type: 'success', message: '登录成功' })
       // 2. 跳转首页
-      // router.replace({ path: '/' })
+      router.replace({ path: '/' })
     }
   })
 }
