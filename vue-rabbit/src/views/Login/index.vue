3c49463a1bf4cf8ab27b6d95fdc56cdea03c4552
yly <2337352929@qq.com>
2025-03-01 19:40:15 +0800

    登录校验

--- vue-rabbit/src/views/Login/index.vue
+++ vue-rabbit/src/views/Login/index.vue
@@ -4,9 +4,9 @@
 
 import { ref } from 'vue'
 
-// import { ElMessage } from 'element-plus'
-// import 'element-plus/theme-chalk/el-message.css'
-// import { useRouter } from 'vue-router'
+import { ElMessage } from 'element-plus'
+import 'element-plus/theme-chalk/el-message.css'
+import { useRouter } from 'vue-router'
 
 // import { useUserStore } from '@/stores/userStore'
 
@@ -28,21 +28,46 @@ const rules = {
     { required: true, message: '密码不能为空', trigger: 'blur' },
     { min: 6, max: 14, message: '密码长度为6-14个字符', trigger: 'blur' },
   ],
-  //   agree: [
-  //     {
-  //       validator: (rule, value, callback) => {
-  //         console.log(value)
-  //         // 自定义校验逻辑
-  //         // 勾选就通过 不勾选就不通过
-  //         if (value) {
-  //           callback()
-  //         } else {
-  //           callback(new Error('请勾选协议'))
-  //         }
-  //       }
-  //     }
-  //   ]
+  agree: [
+    {
+      validator: (rule, value, callback) => {
+        console.log(value)
+        // 自定义校验逻辑
+        // 勾选就通过 不勾选就不通过
+        if (value) {
+          callback()
+        } else {
+          callback(new Error('请勾选协议'))
+        }
+      }
+    }
+  ]
 }
+
+// 3. 获取form实例做统一校验
+const formRef = ref(null)
+// const router = useRouter()
+const doLogin = () => {
+  // const { account, password } = form.value
+  // 调用实例方法
+  formRef.value.validate(async (valid) => {
+    // valid: 所有表单都通过校验  才为true
+    console.log(valid)
+    // 以valid做为判断条件 如果通过校验才执行登录逻辑
+    if (valid) {
+      // TODO LOGIN
+      // await userStore.getUserInfo({ account, password })
+      // 1. 提示用户
+      // ElMessage({ type: 'success', message: '登录成功' })
+      // 2. 跳转首页
+      // router.replace({ path: '/' })
+    }
+  })
+}
+
+// 1. 用户名和密码 只需要通过简单的配置（看文档的方式 - 复杂功能通过多个不同组件拆解）
+// 2. 同意协议  自定义规则  validator:(rule,value,callback)=>{}
+// 3. 统一校验  通过调用form实例的方法 validate -> true
 
 </script>
 
@@ -68,19 +93,19 @@ const rules = {
         </nav>
         <div class="account-box">
           <div class="form">
-            <el-form :model="form" :rules="rules" label-position="right" label-width="60px" status-icon>
+            <el-form ref="formRef" :model="form" :rules="rules" label-position="right" label-width="60px" status-icon>
               <el-form-item prop="account" label="账户">
-                <el-input v-model="form.account"/>
+                <el-input v-model="form.account" />
               </el-form-item>
               <el-form-item prop="password" label="密码">
-                <el-input v-model="form.password"/>
+                <el-input v-model="form.password" />
               </el-form-item>
-              <el-form-item label-width="22px">
-                <el-checkbox size="large">
+              <el-form-item prop="agree" label-width="22px">
+                <el-checkbox size="large" v-model="form.agree">
                   我已同意隐私条款和服务条款
                 </el-checkbox>
               </el-form-item>
-              <el-button size="large" class="subBtn">点击登录</el-button>
+              <el-button size="large" class="subBtn" @click="doLogin">点击登录</el-button>
             </el-form>
           </div>
         </div>
