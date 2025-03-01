91278f526977fea267eac77896272ff125ff0498
yly <2337352929@qq.com>
2025-03-01 17:40:37 +0800

    热榜区

--- vue-rabbit/src/views/Detail/index.vue
+++ vue-rabbit/src/views/Detail/index.vue
@@ -4,6 +4,7 @@ import { getDetail } from '@/apis/detail'
 import { onMounted, ref } from 'vue'
 import { useRoute } from 'vue-router'
 import DetailHot from './components/DetailHot.vue'
+import ImageView from '@/components/ImageView/index.vue'
 
 const goods = ref({})
 const route = useRoute()
@@ -40,6 +41,7 @@ onMounted(() => getGoods())
           <div class="goods-info">
             <div class="media">
               <!-- 图片预览区 -->
+               <ImageView :image-list="goods.mainPictures"/>
 
               <!-- 统计数量 -->
               <ul class="goods-sales">
